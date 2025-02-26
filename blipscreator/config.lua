79014afceb126832b12c6a631f0bc78daa8b4ce5
Natifax <evancayeux@gmail.com>
2025-02-26 18:19:41 +0100

    Add files via upload

---
+++ blipscreator/config.lua
@@ -0,0 +1,7 @@
+ Config = {}
+
+Config.Locale = GetConvar('esx:locale', 'fr')
+
+Config.Blips = {
+	test = {coords = vector3(453.217590, 5572.061524, 795.761230), name = "~y~Blips~s~Testby~p~Natifax~s~", color = 36, sprite = 671}, -----Mont Chiliad-----      
+}
