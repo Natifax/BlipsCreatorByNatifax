2b2090bb7d9d550afd4bc0abe38bf55e3d97583e
Natifax <evancayeux@gmail.com>
2025-02-26 18:32:00 +0100

    Update config.lua

--- blipscreator/config.lua
+++ blipscreator/config.lua
@@ -5,3 +5,18 @@ Config.Locale = GetConvar('esx:locale', 'fr')
 Config.Blips = {
 	test = {coords = vector3(453.217590, 5572.061524, 795.761230), name = "~y~Blips~s~Testby~p~Natifax~s~", color = 36, sprite = 671}, -----Mont Chiliad-----      
 }
+
+------Vous pouvez supprimez si vous voulez------
+#### Pour changer la couleur du nom de blips 
+~r~ = Rouge
+~b~ = Bleu
+~g~ = Vert
+~y~ = Jaune
+~p~ = Violet
+~c~ = Vert
+~m~ = Gris foncé
+~u~ = Noir
+~o~ = Orange Spécial:
+~n~ = Nouvelle Ligne
+~s~ = Réinitialiser la couleur
+~h~ = Texte en gras ####
